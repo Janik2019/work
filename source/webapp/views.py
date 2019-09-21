f2a5bc7b5111fd0ac253dceea1e73e3ca1fc7aac
Janik2019 <lucky-lucky-7777@mail.ru>
2019-09-21 15:07:57 +0600

    Закончил второй этап и создал фикстуры

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import Entry, status_choices
 
-# Create your views here.
+def guestbook_index(request, *args, **kwargs):
+    guestbook = Entry.objects.order_by('-date').filter(status='active')
+    return render(request, 'index.html', context={
+        'guestbook': guestbook,
+    })
