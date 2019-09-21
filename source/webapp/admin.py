c1cb7566df2730c4d2cab3a3870bb3a44e56a460
Janik2019 <lucky-lucky-7777@mail.ru>
2019-09-21 13:45:38 +0600

    Создал модель и записи

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from webapp.models import Entry
+admin.site.register(Entry)
 
 # Register your models here.
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+status_choices =(
+    ('active', 'Активно'),
+    ('blocked', 'Заблокировано'),
+)
+
+
+class Entry(models.Model):
+    name = models.CharField(max_length=20, verbose_name='имя автора')
+    email = models.EmailField(verbose_name='почта')
+    text = models.TextField(max_length=2000, verbose_name='текст записи')
+    date = models.DateTimeField( auto_now_add=True, verbose_name='дата создания')
+    edit = models.DateTimeField(auto_now=True, verbose_name='время редактирования')
+    status = models.CharField(max_length=20, default=status_choices[0][0], verbose_name='Статус', choices=status_choices)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Записи'
+        verbose_name = 'Запись'
